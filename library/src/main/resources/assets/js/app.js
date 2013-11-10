4971c9ece282d20cd1996f715ba4680e1cd2abd9
kalyanisjsu <kalyaninirmal6@gmail.com>
2013-11-09 20:38:52 -0800

    Lab 4 Submission
    Lab 4 Submission

--- library/src/main/resources/assets/js/app.js
+++ library/src/main/resources/assets/js/app.js
@@ -1,16 +1,19 @@
- $(":button").click(function(){
-	 var isbn = this.id;
-	 $(":button").attr("disabled", true)
-	 var request =  $.ajax({
-		 	type: 'put',
-		    url: "http://localhost:8001/library/v1/books/"+ isbn +"?status=lost",
-		    contentType:'application/json'
-		    });
-	 		request.success(function(data, textStatus, jqXHR) {
-		    alert("Status Updated Successfully");
-		    });
-	 		request.error(function(jqXHR, textStatus, errorThrown) {
-			alert("Failed")});
-	 		$(":button").attr("disabled", true)
-	 		
- });
+$(":button").click(function(){
+	var isbn = this.id;
+	var request =  $.ajax({
+		type: 'put',
+		url: "http://localhost:8001/library/v1/books/"+ isbn +"?status=lost",
+		contentType:'application/json',
+		complete: UpadteStatus(isbn)
+	});
+});
+
+function UpadteStatus(isbn){
+	var tempStatus = "#"+isbn+"_status";
+	var tempButton = "#"+isbn;
+	$(tempStatus).html("lost");
+	alert("Status updated succesfully");
+	$(tempButton).attr("disabled", "disabled");
+
+}
+
